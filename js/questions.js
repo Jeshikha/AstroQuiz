1de6c57ece21a5412d3a4bcf40135b1d1bec6a6d
Lee-jr-81 <129674277+Lee-jr-81@users.noreply.github.com>
2023-09-24 15:57:21 +0100

    Added the game logic and coloured the buttons red on incorrect choice

--- js/questions.js
+++ js/questions.js
@@ -1,122 +1,100 @@
 let questions = [
-    {
-        title: "Earth:\nWhat is the average diameter of Earth in kilometers?",
-        nasaId: "PIA00114",
-        choices: [
-            "A) Approximately 12,742 kilometers",
-            "B) Approximately 6,371 kilometers",
-            "C) Approximately 20,000 kilometers",
-            "D) Approximately 16,000 kilometers"
-        ],
-        answer: "B) Approximately 6,371 kilometers",
-        userAnswer: null
-    },
-    {
-        title: "Moon:\nWhat is the average temperature on the Moon's surface?",
-        nasaId: "PIA00225",
-        choices: [
-            "A) -50°C",
-            "B) 20°C",
-            "C) 100°C",
-            "D) -100°C"
-        ],
-        answer: "D) -100°C",
-        userAnswer: null
-    },
-    {
-        title: "Mercury:\nWhich feature on Mercury is similar to the Moon's 'seas' but is named differently?",
-        nasaId: "GSFC_20171208_Archive_e001545",
-        choices: [
-            "A) Oceans",
-            "B) Lakes",
-            "C) Maria",
-            "D) Plains"
-        ],
-        answer: "C) Maria",
-        userAnswer: null
-    },
-    {
-        title: "Venus:\nWhich of the following gases is most abundant in Venus's atmosphere?",
-        nasaId: "GSFC_20171208_Archive_e001734",
-        choices: [
-            "A) Oxygen",
-            "B) Nitrogen",
-            "C) Carbon dioxide",
-            "D) Hydrogen"
-        ],
-        answer: "C) Carbon dioxide",
-        userAnswer: null
-    },
-    {
-        title: "Mars:\nWhich spacecraft successfully landed on Mars in 2021 and began exploring the planet's surface?",
-        nasaId: "PIA25658",
-        choices: [
-            "A) Voyager 1",
-            "B) Curiosity",
-            "C) Hubble",
-            "D) Rosetta"
-        ],
-        answer: "B) Curiosity",
-        userAnswer: null
-    },
-    {
-        title: "Jupiter:\nWhat is the largest moon of Jupiter?",
-        nasaId: "hubble-captures-vivid-auroras-in-jupiters-atmosphere_28000029525_o",
-        choices: [
-            "A) Europa",
-            "B) Io",
-            "C) Ganymede",
-            "D) Callisto"
-        ],
-        answer: "C) Ganymede",
-        userAnswer: null
-    },
-    {
-        title: "Saturn:\nWhat is the name of the largest and most famous ring system around Saturn?",
-        nasaId: "PIA04913",
-        choices: [
-            "A) The Halo Rings",
-            "B) The Belt Rings",
-            "C) The Main Rings",
-            "D) The Saturnian Rings"
-        ],
-        answer: "C) The Main Rings",
-        userAnswer: null
-    },
-    {
-        title: "Uranus:\nWhich unique feature of Uranus sets it apart from most other planets in our solar system?",
-        nasaId: "stsci-h-p1906c-f-514x514.a",
-        choices: [
-            "A) It has no atmosphere",
-            "B) It rotates on its side",
-            "C) It has no moons",
-            "D) It's the hottest planet"
-        ],
-        answer: "B) It rotates on its side",
-        userAnswer: null
-    },
-    {
-        title: "Neptune:\nWhat is the largest storm system on Neptune, similar to Jupiter's Great Red Spot?",
-        nasaId: "PIA02210",
-        choices: [
-            "A) Neptune's Cyclone",
-            "B) Neptune's Hurricane",
-            "C) Neptune's Whirlpool",
-            "D) Neptune's Dark Vortex"
-        ],
-        answer: "D) Neptune's Dark Vortex",
-        userAnswer: null
-    },
-    {
-        title: "Pluto:\nWhich spacecraft conducted a close flyby of Pluto in 2015, providing valuable data about the dwarf planet?",
-        nasaId: "PIA19952",
-        choices: [
-            "A) Cassini",
-            "B) New Horizons",
-            "C) Voyager 2",
-            "D) Kepler"
-        ],
-        answer: "B) New Horizons",
-        userAnswer: null
-    }
+  {
+    title: "Earth:\nWhat is the average diameter of Earth in kilometers?",
+    nasaId: "PIA00114",
+    choices: [
+      "A) Approximately 12,742 kilometers",
+      "B) Approximately 6,371 kilometers",
+      "C) Approximately 20,000 kilometers",
+      "D) Approximately 16,000 kilometers",
+    ],
+    answer: "B",
+    userAnswer: null,
+  },
+  {
+    title: "Moon:\nWhat is the average temperature on the Moon's surface?",
+    nasaId: "PIA00225",
+    choices: ["A) -50°C", "B) 20°C", "C) 100°C", "D) -100°C"],
+    answer: "D",
+    userAnswer: null,
+  },
+  {
+    title:
+      "Mercury:\nWhich feature on Mercury is similar to the Moon's 'seas' but is named differently?",
+    nasaId: "GSFC_20171208_Archive_e001545",
+    choices: ["A) Oceans", "B) Lakes", "C) Maria", "D) Plains"],
+    answer: "C",
+    userAnswer: null,
+  },
+  {
+    title:
+      "Venus:\nWhich of the following gases is most abundant in Venus's atmosphere?",
+    nasaId: "GSFC_20171208_Archive_e001734",
+    choices: ["A) Oxygen", "B) Nitrogen", "C) Carbon dioxide", "D) Hydrogen"],
+    answer: "C",
+    userAnswer: null,
+  },
+  {
+    title:
+      "Mars:\nWhich spacecraft successfully landed on Mars in 2021 and began exploring the planet's surface?",
+    nasaId: "PIA25658",
+    choices: ["A) Voyager 1", "B) Curiosity", "C) Hubble", "D) Rosetta"],
+    answer: "B",
+    userAnswer: null,
+  },
+  {
+    title: "Jupiter:\nWhat is the largest moon of Jupiter?",
+    nasaId:
+      "hubble-captures-vivid-auroras-in-jupiters-atmosphere_28000029525_o",
+    choices: ["A) Europa", "B) Io", "C) Ganymede", "D) Callisto"],
+    answer: "C",
+    userAnswer: null,
+  },
+  {
+    title:
+      "Saturn:\nWhat is the name of the largest and most famous ring system around Saturn?",
+    nasaId: "PIA04913",
+    choices: [
+      "A) The Halo Rings",
+      "B) The Belt Rings",
+      "C) The Main Rings",
+      "D) The Saturnian Rings",
+    ],
+    answer: "C",
+    userAnswer: null,
+  },
+  {
+    title:
+      "Uranus:\nWhich unique feature of Uranus sets it apart from most other planets in our solar system?",
+    nasaId: "stsci-h-p1906c-f-514x514.a",
+    choices: [
+      "A) It has no atmosphere",
+      "B) It rotates on its side",
+      "C) It has no moons",
+      "D) It's the hottest planet",
+    ],
+    answer: "B",
+    userAnswer: null,
+  },
+  {
+    title:
+      "Neptune:\nWhat is the largest storm system on Neptune, similar to Jupiter's Great Red Spot?",
+    nasaId: "PIA02210",
+    choices: [
+      "A) Neptune's Cyclone",
+      "B) Neptune's Hurricane",
+      "C) Neptune's Whirlpool",
+      "D) Neptune's Dark Vortex",
+    ],
+    answer: "D",
+    userAnswer: null,
+  },
+  {
+    title:
+      "Pluto:\nWhich spacecraft conducted a close flyby of Pluto in 2015, providing valuable data about the dwarf planet?",
+    nasaId: "PIA19952",
+    choices: ["A) Cassini", "B) New Horizons", "C) Voyager 2", "D) Kepler"],
+    answer: "B",
+    userAnswer: null,
+  },
 ];
