ad32e2eb40220b5dfa5bcbc66630c84c69810e8e
webtam81 <t.h@tammyharris.co.uk>
2023-09-26 03:18:44 +0100

    pulled sample data from solar system api and put in modal, adjusted questions.js with ids for each planet

--- js/questions.js
+++ js/questions.js
@@ -2,6 +2,7 @@ let questions = [
   {
     title: "Earth:\nWhat is the average diameter of Earth in kilometers?",
     nasaId: "PIA00114",
+    solaireId: "terre",
     choices: [
       "A) Approximately 12,742 kilometers",
       "B) Approximately 6,371 kilometers",
@@ -14,6 +15,7 @@ let questions = [
   {
     title: "Moon:\nWhat is the average temperature on the Moon's surface?",
     nasaId: "PIA00225",
+    solaireId: "lune",
     choices: ["A) -50°C", "B) 20°C", "C) 100°C", "D) -100°C"],
     answer: "D",
     userAnswer: null,
@@ -22,6 +24,7 @@ let questions = [
     title:
       "Mercury:\nWhich feature on Mercury is similar to the Moon's 'seas' but is named differently?",
     nasaId: "GSFC_20171208_Archive_e001545",
+    solaireId: "mercure",
     choices: ["A) Oceans", "B) Lakes", "C) Maria", "D) Plains"],
     answer: "C",
     userAnswer: null,
@@ -30,6 +33,7 @@ let questions = [
     title:
       "Venus:\nWhich of the following gases is most abundant in Venus's atmosphere?",
     nasaId: "GSFC_20171208_Archive_e001734",
+    solaireId: "venus",
     choices: ["A) Oxygen", "B) Nitrogen", "C) Carbon dioxide", "D) Hydrogen"],
     answer: "C",
     userAnswer: null,
@@ -38,6 +42,7 @@ let questions = [
     title:
       "Mars:\nWhich spacecraft successfully landed on Mars in 2021 and began exploring the planet's surface?",
     nasaId: "PIA25658",
+    solaireId: "mars",
     choices: ["A) Voyager 1", "B) Curiosity", "C) Hubble", "D) Rosetta"],
     answer: "B",
     userAnswer: null,
@@ -46,6 +51,7 @@ let questions = [
     title: "Jupiter:\nWhat is the largest moon of Jupiter?",
     nasaId:
       "hubble-captures-vivid-auroras-in-jupiters-atmosphere_28000029525_o",
+    solaireId: "jupiter",
     choices: ["A) Europa", "B) Io", "C) Ganymede", "D) Callisto"],
     answer: "C",
     userAnswer: null,
@@ -54,6 +60,7 @@ let questions = [
     title:
       "Saturn:\nWhat is the name of the largest and most famous ring system around Saturn?",
     nasaId: "PIA04913",
+    solaireId: "saturne",
     choices: [
       "A) The Halo Rings",
       "B) The Belt Rings",
@@ -67,6 +74,7 @@ let questions = [
     title:
       "Uranus:\nWhich unique feature of Uranus sets it apart from most other planets in our solar system?",
     nasaId: "stsci-h-p1906c-f-514x514.a",
+    solaireId: "uranus",
     choices: [
       "A) It has no atmosphere",
       "B) It rotates on its side",
@@ -80,6 +88,7 @@ let questions = [
     title:
       "Neptune:\nWhat is the largest storm system on Neptune, similar to Jupiter's Great Red Spot?",
     nasaId: "PIA02210",
+    solaireId: "neptune",
     choices: [
       "A) Neptune's Cyclone",
       "B) Neptune's Hurricane",
@@ -93,6 +102,7 @@ let questions = [
     title:
       "Pluto:\nWhich spacecraft conducted a close flyby of Pluto in 2015, providing valuable data about the dwarf planet?",
     nasaId: "PIA19952",
+    solaireId: "pluton",
     choices: ["A) Cassini", "B) New Horizons", "C) Voyager 2", "D) Kepler"],
     answer: "B",
     userAnswer: null,
